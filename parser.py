b2f70480a41913a48994aedb7f770d96478a0ced
beaver4 <beaversoftware@mail.ru>
2021-12-14 21:30:43 +0700

    fix class names

--- parser.py
+++ parser.py
@@ -16,25 +16,25 @@ contents = response.text
 soup = BeautifulSoup(contents, 'lxml')
 
  
-temperature_now = soup.find("span", attrs={ "class" : "js_value tab-weather__value_l"}).text.strip() 
+temperature_now = soup.find("span", attrs={ "class" : "unit unit_temperature_c"}).text.strip() 
 #temperature_value_m_element = soup.find("span", attrs={ "class" : "tab-weather__value_m"})
 #if not temperature_value_m_element is None:
 #    temperature_now = temperature_now + temperature_value_m_element.text.strip()
 
-now_info_element = soup.find("div", attrs={ "class" : "now__info nowinfo"})
+now_info_element = soup.find("div", attrs={ "class" : "now-info"})
 
 wind_element = now_info_element.find("div", attrs={ "class" : "unit unit_wind_m_s"})
-wind_speed = wind_element.find("div", attrs={ "class" : "nowinfo__value"}).text.strip()
-wind_direction = wind_element.find("div", attrs={ "class" : "nowinfo__measure nowinfo__measure_wind"}).text.replace("м/с", "").strip()
+wind_speed = wind_element.find(text=True, recursive=False).strip()
+wind_direction = wind_element.find("div", attrs={ "class" : "item-measure"}).text.replace("м/c", "").strip()
 
-pressure = now_info_element.find("div", attrs={ "class" : "unit unit_pressure_mm_hg_atm"}).text.replace("мм рт. ст.", "").strip()
+pressure = now_info_element.find("div", attrs={ "class" : "unit unit_pressure_mm_hg_atm"}).text.replace("ммрт. ст.", "").strip()
 
-humidity = now_info_element.find("div", attrs={ "class" : "nowinfo__item nowinfo__item_humidity"}).text.replace("Влажность", "").replace("%", "").strip()
+#humidity = now_info_element.find("div", attrs={ "class" : "nowinfo__item nowinfo__item_humidity"}).text.replace("Влажность", "").replace("%", "").strip()
 
-magnetic = now_info_element.find("div", attrs={ "class" : "nowinfo__item nowinfo__item_gm"}).find("div", attrs={ "class" : "nowinfo__value"}).text
+#magnetic = now_info_element.find("div", attrs={ "class" : "nowinfo__item nowinfo__item_gm"}).find("div", attrs={ "class" : "nowinfo__value"}).text
 
-temperature_water_element = now_info_element.find("div", attrs={ "class" : "nowinfo__item nowinfo__item_water"})
-temperature_water = temperature_water_element.find("div", attrs={ "class" : "unit unit_temperature_c"}).text.replace("°C", "")
+#temperature_water_element = now_info_element.find("div", attrs={ "class" : "nowinfo__item nowinfo__item_water"})
+#temperature_water = temperature_water_element.find("div", attrs={ "class" : "unit unit_temperature_c"}).text.replace("°C", "")
 
 print("Температура воздуха: " + temperature_now + "°C")
 print("Ветер: " + wind_speed + " м/с " + wind_direction)
